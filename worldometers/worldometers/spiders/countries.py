1ab88523802583ec27e8a46341e5f622f6207646
david-adds <55636118+david-adds@users.noreply.github.com>
2021-12-27 13:42:10 -0300

    code update

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -7,10 +7,10 @@ class CountriesSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        countries = response.xpath('//td/a')
+        countries = response.xpath("//td/a")
         for country in countries:
-            name = response.xpath('.//text()').get()
-            link = response.xpath('.//@href').get()
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
             
             yield{
                 'country_name': name,
